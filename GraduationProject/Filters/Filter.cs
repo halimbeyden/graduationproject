a17bfd54f2bf1f0e3e71a3fd7c066d861a2efef1
halimbeyden <halimbeyden@localhost>
2016-04-03 14:11:40 +0300

    first commit

---
+++ GraduationProject/Filters/Filter.cs
@@ -0,0 +1,87 @@
+﻿using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationProject
+{
+    public class Filter
+    {
+        private static Filter instance;
+
+        private Filter() { }
+
+        public static Filter SharedInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Filter();
+                }
+                return instance;
+            }
+        }
+        public Bitmap DetectRed(Bitmap image)
+        {
+            ColorFiltering cf = new ColorFiltering(new IntRange(150, 255), new IntRange(120, 160), new IntRange(60, 90));
+            return cf.Apply(image);
+        }
+
+        public Bitmap DetectCircle(Bitmap image)
+        {
+
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+            // locate objects using blob counter
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(image);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            // create Graphics object to draw on the image and a pen
+            Graphics g = Graphics.FromImage(image);
+            Pen redPen = new Pen(Color.Red, 2);
+            // check each object and draw circle around objects, which
+            // are recognized as circles
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+                try
+                {
+                    shapeChecker.CheckShapeType(edgePoints);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                List<IntPoint> corners;
+                AForge.Point center;
+                float radius;
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(redPen,
+                        (int)(center.X - radius),
+                        (int)(center.Y - radius),
+                        (int)(radius * 2),
+                        (int)(radius * 2));
+                }
+                else if (shapeChecker.IsQuadrilateral(edgePoints, out corners))
+                {
+                    g.DrawPolygon(redPen, ToPointsArray(corners));
+                }
+            }
+            redPen.Dispose();
+            g.Dispose();
+            return image;
+        }
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            return points.Select(p => new System.Drawing.Point(p.X, p.Y)).ToArray();
+        }
+    }
+}
---
+++ GraduationProject/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using AForge;
+
+namespace GraduationProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pb1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pb1.ImageLocation = ofd.FileName;
+                pb1.Refresh();
+            }
+        }
+
+        private void processBtn_Click(object sender, EventArgs e)
+        {
+            //pb2.Image = Filter.SharedInstance.DetectRed(new Bitmap(pb1.Image));
+            pb2.Image = Filter.SharedInstance.DetectCircle(new Bitmap(pb1.Image));
+            pb2.Refresh();
+        }
+    }
+}
