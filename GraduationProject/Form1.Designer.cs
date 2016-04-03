579c273b64f08bf59888b7b69e5ba9662c5bf251
Halim Beyden <halim.beyden@gmail.com>
2016-04-03 14:11:40 +0300

    first commit

---
+++ GraduationProject/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GraduationProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.processBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(0, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(600, 563);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(606, 0);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(634, 563);
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(0, 569);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // processBtn
+            // 
+            this.processBtn.Location = new System.Drawing.Point(606, 569);
+            this.processBtn.Name = "processBtn";
+            this.processBtn.Size = new System.Drawing.Size(75, 23);
+            this.processBtn.TabIndex = 3;
+            this.processBtn.Text = "Process";
+            this.processBtn.UseVisualStyleBackColor = true;
+            this.processBtn.Click += new System.EventHandler(this.processBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 623);
+            this.Controls.Add(this.processBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button processBtn;
+    }
+}
+
